50e5edfc44f979aa75f0d892bd2529a94a7bd16f
KawaruVN <huuphuc1208vn@gmail.com>
2026-09-11 15:43:49 +0700

    Update logger.py

--- src/customs_ai/logger.py
+++ src/customs_ai/logger.py
@@ -1,1 +1,11 @@
+import logging
+
+from customs_ai.config import settings
+
+logging.basicConfig(
+    level=getattr(logging, settings.log_level),
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
+logger = logging.getLogger("customs_ai")
 
